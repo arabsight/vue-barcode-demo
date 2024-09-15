a01f37af1e15a3958a8ed05275f973dd1a8aa7fa
Rabah Ghodbane <r-ghodbane@live.com>
2024-09-15 21:19:47 +0100

    First one

---
+++ src/components/SVGBarcode.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg id="barcode"></svg>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import JsBarcode from 'jsbarcode';
+
+const props = defineProps<{ value: string | number }>();
+
+onMounted(() => {
+  JsBarcode('#barcode', props.value.toString(), {
+    format: 'code39',
+    height: 50,
+    displayValue: false,
+    margin: 0
+  });
+});
+</script>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>
+    <SVGBarcode :value="562" />
+  </main>
+</template>
+
+<script setup lang="ts">
+import SVGBarcode from '@/components/SVGBarcode.vue';
+</script>

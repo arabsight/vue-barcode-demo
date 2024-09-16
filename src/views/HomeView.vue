f244b8e99d560c7aa61e54247d657a70c7660859
Rabah Ghodbane <r-ghodbane@live.com>
2024-09-16 22:34:42 +0100

    second

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,31 @@
 <template>
   <main>
-    <SVGBarcode :value="562" />
+    <div ref="cardRef">
+      <SVGBarcode :value="562" />
+    </div>
+
+    <button @click="handlePrint">Print</button>
   </main>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import SVGBarcode from '@/components/SVGBarcode.vue';
+import { useVueToPrint } from 'vue-to-print';
+
+const cardRef = ref();
+
+const { handlePrint } = useVueToPrint({
+  content: cardRef,
+  documentTitle: 'card',
+  removeAfterPrint: true
+});
 </script>
+
+<style scoped>
+@page {
+  margin: 10px;
+  /* background-color: blueviolet;
+  width: 300px; */
+}
+</style>
